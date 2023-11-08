9a7b130be83b25ecc24ccc34854224969db75c75
kentheboy <foryoukentheboy>
2023-11-08 23:09:41 +0900

    add product get list req

--- app/Http/Controllers/Khufu/ProductController.php
+++ app/Http/Controllers/Khufu/ProductController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Khufu;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Khufu\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function list() {
+        $products = Product::where('status', 1)->get();
+        return $products;
+    }
 }
