5f4782b9a7f9e0189985eea391435894ee5ce720
kentheboy <foryoukentheboy>
2023-11-08 22:22:55 +0900

    add Products Table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $faker = Faker::create();
+        foreach(range(1, 10) as $index) {
+            DB::table('products')->insert([
+                'name' =>  $faker->word,
+                'description' => $faker->text($maxNbChars = 200),
+                'price' => $faker->numberBetween($min = 1000, $max = 5000),
+                'status' => 1,
+                'created_at' => now(),
+                'updated_at' =>now()
+            ]);
+        }
     }
 }
